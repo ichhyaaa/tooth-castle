13eb1ed5051746927e283b9ce050a389485b1ee9
ichhyaaa <129594131+ichhyaaa@users.noreply.github.com>
2023-12-17 14:04:37 +0545

    Third commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Login } from './pages/login';
-import { SignUp } from './pages/signup';
+import SignUp from './pages/signup';
+import Dashboard from './pages/dashboard'
 
 function App() {
   return (
@@ -10,8 +11,13 @@ function App() {
       <Routes>
         <Route path='/login' element={<Login/>}></Route>
         <Route path='/signup' element={<SignUp/>}></Route>
+        <Route path='/dashboard' element={<Dashboard/>}></Route>
+        <Route path='/dashboard' element={<Dashboard/>}></Route>
+                <Route path='/dashboard' element={<Dashboard/>}></Route>
+
 
       </Routes>
+
     </BrowserRouter>
   );
 }
--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -1,34 +1,124 @@
+import React, { useState } from "react";
 
-export const SignUp =()=>{
-    return(
-        <div>
-            <h1> Tooth Castle</h1>
-            <h2> Sign Up</h2>
 
-        <div class="field input-feild">
-                        <i class="fa fa-user"></i>
-                        <input type="text" placeholder="Full Name" class="input"/>
-                    </div>
+export default function SignUp() {
+    return (
+      <>
+        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+          <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+            <img
+              className="mx-auto h-10 w-auto"
+              src=""
+              alt="Dental Clinic"
+            />
+            <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+              Sign Up 
+            </h2>
+          </div>
+  
+          <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+            <form className="space-y-6" action="#" method="POST">
+              <div>
+                <label htmlFor="fullname" className="block text-sm font-medium leading-6 text-gray-900">
+                  Full Name
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="fullname"
+                    name="fullname"
+                    type="fullname"
+                    autoComplete="fullname"
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
 
-                    <div class="field input-feild">
-                        <i class="fa fa-at"></i>
-                        <input type="text" placeholder="Email" class="input"/>
-                    </div>
+              <div>
+                <label htmlFor="phonenumber" className="block text-sm font-medium leading-6 text-gray-900">
+                  Phone number
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="phonenumber"
+                    name="phonenumber"
+                    type="phonenumber"
+                    autoComplete="phonenumber"
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
 
-                    <div class="field input-feild">
-                        <i class="fa fa-lock"></i>
-                        <input type="password" placeholder="Password" class="input"/>
-                    </div>
-        
-                    <div class="field input-feild">
-                        <button> Sign in <a href="signup.js"> </a></button>
+              <div>
+                <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                  Email address
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="email"
+                    name="email"
+                    type="email"
+                    autoComplete="email"
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
 
-                    </div>       
-            
-                    <div class="link">
-                    <p>By clicking the Sign Up you agree <br/>
-                     <a href="#">Term and Condition</a> and <a href="#">Policy  Privacy</a> of Tooth Castle  </p> 
-                    </div>
-                    </div>
+              <div>
+                <div className="flex items-center justify-between">
+                  <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                    Create Password
+                  </label>
+                </div>
+                <div className="mt-2">
+                  <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    autoComplete="current-password"
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+  
+              <div>
+                <div className="flex items-center justify-between">
+                  <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                    Confirm Password
+                  </label>
+                </div>
+                <div className="mt-2">
+                  <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    autoComplete="current-password"
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+  
+              <div>
+                <button
+                  type="submit"
+                  className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+                  Register
+                </button>
+              </div>
+            </form>
+  
+            <p className="mt-10 text-center text-sm text-gray-500"> 
+              <a href="" className="mt-10 text-center text-sm text-gray-500 font-semibold leading-6 text-indigo-600 hover:text-indigo-500"> 
+              I am already member
+              </a>
+            </p>
+          </div>
+        </div>
+      </>
     );
-};
+  };
